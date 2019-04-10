382df2bb7c189351aa4600c65c241abff3a43d7d
unknown <ulyana250188@gmail.com>
2019-04-10 10:33:06 +0300

    Updates

---
+++ precache-manifest.2e3e3f04bc45a907de9b232bdf8aa8df.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "0897ef850b738de30ab2",
+    "url": "/login-app.git/static/js/runtime~main.15dcd607.js"
+  },
+  {
+    "revision": "792bb1f800f868abbc8f",
+    "url": "/login-app.git/static/js/main.74592daa.chunk.js"
+  },
+  {
+    "revision": "dde1f47a294ca2e597433e249f63e09d",
+    "url": "/login-app.git/index.html"
+  }
+];
