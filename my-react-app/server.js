dd18a5a5c8cb6244f41264899f469ede3500589b
unknown <ulyana250188@gmail.com>
2019-04-10 11:03:55 +0300

    react-app from server

---
+++ my-react-app/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static('build'));
+
+app.listen(port, () => {
+  console.log(`Open http://127.0.0.1:${port}`)
+});
